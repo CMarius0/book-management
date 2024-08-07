2f0dc5ecd5eaf7713fd9d369217bb0db8cc10b8e
CMarius0 <mariuscacior@gmail.com>
2024-08-07 19:32:09 +0300

    Added functionality for editing and deleting entries

--- src/components/BookForm.tsx
+++ src/components/BookForm.tsx
@@ -1,52 +1,59 @@
+import { useState } from "react";
 import "../styles/BookForm.css"
 import Book from "../types";
-import { add_book } from "../books";
+import { Form, Navigate, redirect, useLoaderData, useNavigate  } from "react-router-dom";
 
-export function loader({params}:{params:any}){
 
-}
+
 
 
 function BookForm () {
+    const navigate = useNavigate();
+    const {book, submit} = useLoaderData() as {book:Book, submit:Function};
 
-    let handlesubmit = (event:any) => {
-        event.preventDefault();
-        let book:Book = {
-            name: event.target.name.value,
-            author: event.target.author.value,
-            genre: event.target.genre.value,
-            date: event.target.date.value,
-        }
-        add_book(book);
+    //test function
+    console.log(book.id,book.name)
+
+    const [ name, setName ] = useState(String(book.name));
+    const [ author, setAuthor ] = useState(String(book.author));
+    const [ genre, setGenre ] = useState(String(book.genre));
+    const [ date, setDate ] = useState(String(book.date));
+
+
+    function HandleChange(e:any, setFunction:React.Dispatch<React.SetStateAction<string>>) {
+        setFunction(e.target.value);
     }
+
+
     return (
     <>  <div id="BookForm">
-            <form onSubmit={handlesubmit}>
+            <Form onSubmit={(event:any) =>{submit(event); navigate("/")}} method="post" id="form">
                 <div id="title"> Add / Modify Book</div>
+                <input id="id" name="id" type="number"  value={Number(book.id)}></input>
                 <section>
-                <div className="input">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" name="name"></input>
-                </div>
-                <div className="input">
-                <label htmlFor="name">Author</label>
-                <input type="text" name="author"></input>
-                </div>
+                    <div className="input">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" value={name} onChange={(e:any)=>{HandleChange(e,setName)}}></input>
+                    </div>
+                    <div className="input">
+                        <label htmlFor="name">Author</label>
+                        <input type="text" name="author" value={author} onChange={(e:any)=>{HandleChange(e,setAuthor)}}></input>
+                    </div>
                 </section>
                 <section>
-                <div className="input">
-                <label htmlFor="name">Genre</label>
-                <input type="text" name="genre"></input>
-                </div>
-                <div className="input">
-                <label htmlFor="name">Publication Date</label>
-                <input type="text" name="date" placeholder="YYYY-MM-DD"></input> 
-                </div>
+                    <div className="input">
+                        <label htmlFor="name">Genre</label>
+                        <input type="text" name="genre" value={genre} onChange={(e:any)=>{HandleChange(e,setGenre)}}></input>
+                    </div>
+                    <div className="input">
+                        <label htmlFor="name">Publication Date</label>
+                        <input type="text" name="date" placeholder="YYYY-MM-DD" value={date} onChange={(e:any)=>{HandleChange(e,setDate)}}></input> 
+                    </div>
                 </section>
                 
                 
                 <button id="submit" type="submit">Save</button>
-            </form>
+            </Form>
         </div>
     </>
     );
